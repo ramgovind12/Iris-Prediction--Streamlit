10683a69b8cc18c286478481e1fc91664bbcd246
ramgovind12 <ramgovindvi.in@gmail.com>
2022-07-15 19:49:36 +0530

    Modified

--- iris/iris/predict/views.py
+++ iris/iris/predict/views.py
@@ -1,5 +1,21 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 def predict(request):
     return render(request, 'predict.html')
+
+def predict_changes(request):
+    if request.POST.get('action') == 'post':
+        sepal_length = float(request.POST.get('sepal_length'))
+        sepal_width = float(request.POST.get('sepal_width'))
+        petal_length = float(request.POST.get('petal_length'))
+        petal_width = float(request.POST.get('petal_width'))
+
+        model = pd.read_pickle('predict')
+        result = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+
+        classification = result[0]
+
+        return JsonResponse({'result': classification,'sepal_length':sepal_length,'sepal_width':sepal_width,
+        'petal_length':petal_length,'petal_width':petal_width},safe = False)
